53220d52ffb7d2497f3dc1cbbf3747a0326191b0
marianita13 <124885480+marianita13@users.noreply.github.com>
2023-08-27 15:36:54 -0500

    feat :sparkles: video information show video information and try to put a video over the image

--- js/main.js
+++ js/main.js
@@ -1,50 +1,60 @@
+document.addEventListener("DOMContentLoaded", () =>{
 // FUNCION PARA CONSUMIR LA API DE LOS IMAGEN VIDEOS Y PONERLOS EN EN HTML
-let informacion = async() =>{
-    const responseChannel = await fetch("channel.json");
-    let responseVideos = await fetch("videos.json");
-    let videos = await responseVideos.json();
-    let canal = await responseChannel.json();
+    let informacion = async() =>{
+        const responseChannel = await fetch("channel.json");
+        let responseVideos = await fetch("videos.json");
+        let videos = await responseVideos.json();
+        let canal = await responseChannel.json();
 
-    let banner = document.querySelector(".container")
-    console.log(videos.contents[0].video.movingThumbnails[0].url);
-        banner.insertAdjacentHTML("beforeend", /*HTML*/`
-        <div class="banner">
-        <img src="${canal.banner.desktop[3].url}" class="fondo-grande">
-        </div>
-        <div class="list-container">
-            ${videos.contents.map((value) => /*HTML*/`
-                <div class="vid-list">
-                    <div class="contenedor-video">
-                        <img src="${value.video.thumbnails[3].url}" class="imagen-principal">
-                        <video class="video" scr="./images/video.mp4" controls></video>
-                    </div>
-                    <div  class="flex-div">
-                        <img src="${canal.avatar[1].url}" class="imagen-video">
-                        <div>
-                            <a href="">${value.video.title}</a>
-                            <p>${value.video.stats.views} views ·${value.video.publishedTimeText}</p>
+        let banner = document.querySelector(".container")
+        console.log(videos.contents[0].video.movingThumbnails[0].url);
+            banner.insertAdjacentHTML("beforeend", /*HTML*/`
+            <img src="${canal.banner.desktop[3].url}" class="fondo-grande">
+            <div class="list-container">
+                ${videos.contents.map((value) => /*HTML*/`
+                    <div class="vid-list">
+                        <div class="contenedor-video">
+                        <a href="single.html" class="a-principal"><img src="${value.video.thumbnails[3].url}" class="imagen-principal"></a>
+                            <video class="video" src="./images/video.mp4" controls></video>
+                        </div>
+                        <div  class="flex-div">
+                            <img src="${canal.avatar[1].url}">
+                            <div>
+                                <a href="">${value.video.title}</a>
+                                <p>${value.video.stats.views} views ·${value.video.publishedTimeText}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            `).join("")}
-        </div>
-    `)
-}
-informacion();
+                `).join("")}
+            </div>
+        `)
+    }
+    informacion();
 
 //FUNCION PARA QUE SE REPRODUZAC UNA PEQUEÑA PARTE DEL VIDEO
-const imagen = document.querySelector(".imagen-principal")
-const video = document.querySelector(".video")
+    const contenedorVideos = document.querySelectorAll(".contenedor-video");
+    console.log("prueba1");
 
-imagen.addEventListener("mouseover", () =>{
-    imagen.style.display="none"
-    video.style.display="block"
-    video.play()
-})
-imagen.addEventListener("mouseout", () =>{
-    imagen.style.display="block"
-    video.style.display="none"
-    video.pause()
+        contenedorVideos.forEach(contenedor => {
+            const imagen = contenedor.querySelector(".imagen-principal");
+            const video = contenedor.querySelector(".video");
+            console.log("prueba2");
+
+            contenedor.addEventListener("mouseover", () => {
+                imagen.style.display = "none";
+                video.style.display = "block";
+                video.play();
+                console.log("dentro");
+            });
+
+            contenedor.addEventListener("mouseout", () => {
+                imagen.style.display = "block";
+                video.style.display = "none";
+                video.pause();
+                console.log("fuera");
+            });
+        }
+    );
 })
 
 
@@ -78,4 +88,8 @@ const toggleClass = (elements, className) => {
       toggleClass([Trending, Music, Live, Gaming, News, Sports, Learning, premium, studio, Musica, kids, suscripcion,Settings,report,Help,Send]);
     }, 250);
   });
+
+
+  //FUNCION PARA PASAR LA INFORMACION DE UN HTML A OTRO
+  
   
